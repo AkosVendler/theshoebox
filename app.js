5a2f3060e21d15fb10db3a0434a8c6a94a6422a2
TheShoeBox <73746378+AkosVendler@users.noreply.github.com>
2023-02-05 15:59:00 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+function fizetendo() {
+    var NikeAirForce1 = 47421
+    var AirJordan1Mid = 51459
+
+    var ossz = 0;
+    var menny = 0;
+    var key = "";
+    key = localStorage.key(i);
+    menny = Number(localStorage.getItem(key));
+    var x = localStorage.getItem(key);
+    document.getElementById("total").innerHTML = (window[key] * menny + "Ft");
+    document.getElementById("osszes").innerHTML = (window[key] * menny + 1500 + "Ft");
+}
