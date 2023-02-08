59958e02b08cc26da2f8dd494a61f324b6d45445
Szeby918 <123585020+Szeby918@users.noreply.github.com>
2023-02-08 21:43:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var slider_img = document.querySelector('.slider-img');
 var slider_img1 = document.querySelector('.slider-img1');
+var slider_img2 = document.querySelector('.slider-img2');
+var slider_img3 = document.querySelector('.slider-img3');
 var images = ['1.png', '2.png', '3.png', '4.png'];
 var img = ['jordan1.png', 'jordan1-2.png', 'jordan1-3.png', 'jordan1-4.png']
+var images2 = ['KUKI.png', 'KUKI oldalrol.png' , 'KUKI felülröl.png' ,'KUKI hátulrol.png' ]
+var images3 = ['Főkép.png', 'Kép1.png', 'Kép2.png', 'Képhátul.png']
 var i = 0;
 
 
@@ -19,6 +23,40 @@ function next(){
 
 function setImg(){
     return slider_img.setAttribute('src', 'img/' + images[i]);
+}
+
+
+function prev2(){
+    if(i <= 0 ) i = images.length;
+    i--;
+    return setImg2();
+}
+
+function next2(){
+    if (i >= images.length -1) i = -1;
+    i++;
+    return setImg2();
+}
+
+function setImg2(){
+    return slider_img2.setAttribute('src', 'img/' + images2[i]);
+}
+
+
+function prev3(){
+    if(i <= 0 ) i = images.length;
+    i--;
+    return setImg3();
+}
+
+function next3(){
+    if (i >= images.length -1) i = -1;
+    i++;
+    return setImg3();
+}
+
+function setImg3(){
+    return slider_img3.setAttribute('src', 'img/' + images3[i]);
 }
 
 function setImg1(){
@@ -40,6 +78,22 @@ function next1(){
 //kosár
 var NikeAirForce1 = 47500
 var AirJordan1Mid = 50000
+var NikeAirMax270 = 58500
+var NikeAirForceBlueGray =45500
+var YeezySlideOnyx = 49500
+var YeezySlideGlowGreen = 65000
+var YeezyFoamRNNRSand = 93000
+var YeezyFoamRNNRRed = 142000
+var YeezyBoost350V2BlackRed = 158000
+var YeezyBoost350V2DazzlingBlue = 120000
+var YeezyBoost350V2Oreo = 165000
+var AirJordan1Low = 49500
+var AirJordan1MidDiamond = 120000
+var AirJordan1Mid = 93000
+var AirJordan11CMFTLow = 70000
+var AirJordan1LowSE = 60000
+var JordanAccess = 30000
+var Zion2 = 65000
 
 function hozzáad(cucc) {
     
